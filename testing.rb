6283711b81f56f77dbed8d20c30c4569128332b2
Dino Reic <rejotl@gmail.com>
2015-06-05 14:21:52 +0200

    master: work

--- testing.rb
+++ testing.rb
@@ -1,45 +1,119 @@
+require 'json'
+require 'pp'
+
 class MasterApi
 
   @@opts = {}
   @@actions = {}
 
-  def self.params(key, value)
+  def self.params(key, *values)
     @@opts[:params] ||= {}
-    @@opts[:params][key] = value
+    @@opts[:params][key] = *values
   end
 
   def self.name(name)
     @@opts[:name] = name
   end
 
-  def self.action(name, &block)
-    # opts[:block] = &block
-    @@opts[:action] = opts
+  def self.description(data)
+    @@opts[:description] = data
   end
 
-  def run(what, options)
+  def self.proc(&block)
+    @@opts[:proc] = block
+  end
 
+  def self.action(proc_name)
+    proc = yield
+    @@actions[proc_name] = @@opts.dup
+    @@actions[proc_name][:proc] = proc
+    @@opts = {}
+  end
+
+  def self.inline_action(proc_name, name=nil, &block)
+    @@actions[proc_name] = {}
+    @@actions[proc_name][:name] = name
+    @@actions[proc_name][:proc] = block
+    @@opts = {}
+  end
+
+  def self.run(proc_name, options={})
+    @response = {}
+    @error = {}
+    @message  = nil
+    res = @@actions[proc_name] ? @@actions[proc_name][:proc].call : new.send(proc_name)
+    res = res.call if res.kind_of?(Proc)
+    @response[:data] = res
+    @response[:message] = @message if @message
+    @response[:error] = @error if @error
+    @response[:ip] = '127.0.0.1'
+    @response
+  end
+
+  def self.actions
+    instance_level_actions = UserApi.instance_methods - Object.instance_methods    
+    @@actions.keys + instance_level_actions
+  end
+
+  def self.action_details(name)
+    @@actions[name] ||= {}
+    details = @@actions[name].reject { |key| [:proc].index(key) }
+    details[:name] ||= "#{name.to_s} action"
+    details
   end
 
 end
-
-
-
-
 
 
 class UserApi < MasterApi
 
-  params email: [:email, :req], id: :integer
-  action :show, { name:'Show user data',  description:'Shows user data' } do
-    return 12344
+  action :show do
+    name        'Show user data'
+    params      :email, :email, :req
+    params      :id, :integer
+    lambda do
+      return 12344
+      2222
+    end
   end
 
-  action :index do
-    return 1234435345345
+  def inline
+    'inline'
+  end
+
+  # inline_action :index
+  #   lambda do
+  #     return 1
+  #     2
+  #   end
+  # end
+
+  # inline_action :index do -> {
+  #     return 1
+  #     2
+  # } end
+
+  inline_action :index do
+      @error =   'some error'
+      next 1
+      2
+  end
+
+  inline_action :name do
+    @message = 'name added'
+    'Dux'
   end
 
 end
 
+pp UserApi.run(:show, { email:'rejotl@gmail.com'})
+pp UserApi.run(:index, { email:'rejotl@gmail.com'})
+pp UserApi.run(:name)
+pp UserApi.run(:inline)
 
-UserApi.run(:show, { email:'rejotl@gmail.com'})
+puts 'Desc'
+
+for el in UserApi.actions
+  pp UserApi.action_details(el)
+end
+
