0168ec694efcd9df3d876f68bbce8c9f4ddd187b
Dino Reic <rejotl@gmail.com>
2015-06-05 08:08:15 +0200

    master: api eorks

--- testing.rb
+++ testing.rb
@@ -1,22 +1,20 @@
 class MasterApi
 
-  @@methods = {}
+  @@opts = {}
+  @@actions = {}
 
-  def self.params(opts)
-    @@last_params = opts
+  def self.params(key, value)
+    @@opts[:params] ||= {}
+    @@opts[:params][key] = value
   end
 
   def self.name(name)
-    @@last_name = name
+    @@opts[:name] = name
   end
 
-  def self.action(name, opts={}, &block)
+  def self.action(name, &block)
     # opts[:block] = &block
-    @@methods[name] = opts
-    @@methods[:params] = @@last_params if @@last_params
-    @@methods[:name]   = @@last_name if @@last_name
-    @@last_params = nil
-    @@last_name = nil
+    @@opts[:action] = opts
   end
 
   def run(what, options)
@@ -35,6 +33,10 @@ class UserApi < MasterApi
   params email: [:email, :req], id: :integer
   action :show, { name:'Show user data',  description:'Shows user data' } do
     return 12344
+  end
+
+  action :index do
+    return 1234435345345
   end
 
 end
