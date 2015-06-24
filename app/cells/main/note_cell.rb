7301485acdc83241c860c67f6549b0a8655f0075
Dino Reic <rejotl@gmail.com>
2015-06-24 09:15:16 +0200

    master: app work

--- app/cells/main/note_cell.rb
+++ app/cells/main/note_cell.rb
@@ -8,4 +8,8 @@ class Main::NoteCell < LuxCell
     @note = Note.get(id)
   end
 
+  def archive
+    @notes = Note.unscoped.order('updated_at desc').my.where('active=?', false).paginate(30)
+  end
+
 end
